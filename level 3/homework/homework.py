0784fdfaf73c7e18af9149b00708e7a8a16e0e9b
an619 <anirazmadze06@gmail.com>
2024-06-28 01:07:54 +0400

    GOA

---
+++ level 3/homework/homework.py
@@ -0,0 +1,7 @@
+# 1) შექმენი ცვლადები,  სახელი,გვარი,ასაკი,ჰობი,ქვეყანა და  ეს ყველა ცვლადი დაპრინტეთ. 
+name = "Ani"
+last_name = "Razmadze"
+age = "18"
+hobbys = "reading books"
+country = "Georgia"
+print(name , last_name , age , hobbys , country)
