fab38beafe274f905dd51ac6ab74581c6dfbfdbd
an619 <anirazmadze06@gmail.com>
2024-06-28 01:02:56 +0400

    GOA

---
+++ level 3/homework/homework1.py
@@ -0,0 +1,7 @@
+# 2) შექმენით ცვლადები, გადაეცით მნიშვნელობები და შემდგომ ააწყვეთ ერთი დიდი წინადადება.
+name = "Ani"
+last_name = "Razmadze"
+age = "18"
+hobbys = "watching movies"
+
+print("my name is", name , last_name + ", i am" , age , "years old and my hobby is" , hobbys + ".")
