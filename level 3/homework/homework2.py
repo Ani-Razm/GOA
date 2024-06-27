32a45424bcbea541d35a4c2037765c53023ba781
an619 <anirazmadze06@gmail.com>
2024-06-28 00:57:56 +0400

    GOA

---
+++ level 3/homework/homework2.py
@@ -0,0 +1,26 @@
+#3)  გააკეთეთ 20 მაგალითი ყველა მათემათიკურ ოპერატორებზე. +,-,*,//,**
+
+print(2 + 3)
+print(3 + (-2))
+print(2.3 + 2)
+print(9 + 9)
+
+print(5 - 5)
+print(10 - 20)
+print(4.5 - 3)
+print(20 - 9)
+
+print(2 * 2)
+print(7 * 2)
+print(3 * 9)
+print(2 * (-2))
+
+print(10 // 2)
+print(15 // 3)
+print(3 // 2)
+print(10 // 4)
+
+print(3 ** 2)
+print(15 ** 3)
+print(10 ** 2)
+print(2 ** 90)
