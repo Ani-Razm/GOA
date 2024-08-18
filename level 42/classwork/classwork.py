ec6c9e538efba89d7e2ae467ea3bb875656f7e35
an619 <anirazmadze06@gmail.com>
2024-08-18 22:58:32 +0400

    GOA

--- level 42/classwork/classwork.py
+++ level 42/classwork/classwork.py
@@ -1,5 +1,43 @@
-#1
+#1 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 def remove_exclamation_marks(s):
     return s.replace("!", "")
 
 #2
+def reverse(st):
+    rev = []
+    for word in st.split():
+        rev.insert(0, word)
+    return " ".join(rev)
+
+
+#3
+def distinct(seq):
+    result = []
+    for num in seq:
+        if num not in result:
+            result.append(num)
+    return result
+
+#4
+def is_isogram(string):
+    string = string.lower()
+    for i in string:
+        if string.count(i) > 1:
+            return False
+    return True
+
+#5
+def remove_duplicate_words(s):
+    result = []
+    for word in s.split():
+        if word not in result:
+            result.append(word)
+    return " ".join(result)
+
+#6
+def vaporcode(s):
+    return s.upper().replace(" ", "").replace("", "  ").strip("  ")
+
+#7
+
+
---
+++ level 42/homework/homework.py
@@ -0,0 +1,18 @@
+#1
+def to_jaden_case(string):
+    return " ".join(word.capitalize() for word in string.split())
+
+#2
+def stray(arr):
+    for i in arr:
+        if arr.count(i) == 1:
+            return i
+        
+#3
+def dup(arry):
+    arry = " ".join(arry)
+    new = ""
+    for i in range(len(arry) - 1):
+        if arry[i] != arry[i + 1]:
+            new += arry[i]    
+    return (new + arry[-1]).split()  
