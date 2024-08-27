70f83f07358f7e1e95af4b1f1cbfa89989d6cbea
an619 <anirazmadze06@gmail.com>
2024-08-28 01:19:15 +0400

    GOA

--- level 48/homework/homework.py
+++ level 48/homework/homework.py
@@ -35,4 +35,3 @@ def order(sentence):
     
     return " ".join(result)
 
-print("".join(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']))
