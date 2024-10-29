7766c9e13c10cf446cdd41114ba01f07a34e9154
Ani <158804664+an619@users.noreply.github.com>
2024-10-29 19:52:27 +0400

    GOA

--- level 72/classwork/index.js
+++ level 72/classwork/index.js
@@ -1,4 +1,14 @@
-setInterval(function(){
-    let name = document.getElementById('name');
-    name.textContent += "ani";
-}, 3000)
+let time = 20;
+let timer = document.getElementById("timer");
+
+const timeChange = setInterval(
+    function () {
+        time --;
+        timer.textContent = `${time} left`;
+        if(time == 0) {
+            timer.textContent = "time is up";
+            clearInterval(timeChange);
+        };
+    }, 1000
+)
+
--- level 72/homework/index1.js
+++ level 72/homework/index1.js
@@ -1,15 +1,36 @@
+//5
 let start = document.getElementById("start");
 let stop = document.getElementById("stop");
 let reset = document.getElementById("reset");
-let timer = document.getElementById("timer")
+let timer = document.getElementById("timer");
 
-let seconds = 10;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let timer_change;
 
-start.addEventListener("click", setInterval(function() {
-    seconds--;
-    minutes = Math.trunc(seconds / 60);
-    timer.textContent = (`00:${minutes}:${seconds % 60}`);
-    if (seconds < 0) {
-        clearInterval();
-    }
-}, 1000));
+start.onclick = function () {
+        timer_change = setInterval(function(){
+        seconds += 1
+        if(seconds == 60){
+            seconds = 0;
+            minutes += 1;
+        }
+        if(minutes == 60){
+            minutes = 0;
+            hours += 1;
+        }
+        timer.textContent = `${hours}:${minutes}:${seconds}`;
+    }, 1000)
+};
+
+reset.onclick = function () {
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    timer.textContent = `${hours}:${minutes}:${seconds}`
+}
+
+stop.onclick = function() {
+    clearInterval(timer_change);
+};
