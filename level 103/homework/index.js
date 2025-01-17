3eb6fcdb4ed7954af951d3e4b76222bee7d37fcb
Ani <158804664+an619@users.noreply.github.com>
2025-01-17 17:29:44 +0400

    GOA

--- level 103/homework/index.js
+++ level 103/homework/index.js
@@ -2,11 +2,31 @@ const colorPicker = document.querySelector('input[type="color"]');
 const numInput = document.querySelector('#number');
 const nameInput = document.querySelector('#name');
 const numBtn = document.querySelector('.main-screen button');
-const nameBtn = document.querySelector
+const mainScreen = document.querySelector('.main-screen');
+const logInScreen = document.querySelector('.log-in-screen.active');
+const nameBtn = document.querySelector('.log-in-screen button');
 
-let score = 0;
+let userName = 'Guest';
+
+//log in page
+nameBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    if(nameInput.value.length > 0){
+        userName = nameInput.value;
+        mainScreen.classList.add('active');
+        logInScreen.classList.remove('active');
+        document.querySelector('.userName span').innerHTML = userName;
+    }
+});
+
+// guessing number page
+let score = localStorage.getItem('score') || 0;
+document.querySelector('.userScore').querySelector('span').textContent = score;
+
 let guessNumRange = localStorage.getItem('guessNumRange') || 2 ;
 let correctNum = localStorage.getItem('correctNum') || Math.trunc(Math.random() * guessNumRange);
+
+
 savetoLocalS({name: "correctNum", value: correctNum})
 savetoLocalS({name: 'guessNumRange', value: guessNumRange})
 
@@ -42,11 +62,21 @@ function loadInfo() {
 function checkIfCorrectAnswer() {
     console.log(correctNum);
     if(numInput.value == correctNum){
+        alert('correct')
         score++;
         guessNumRange *= 2;
         correctNum = Math.trunc(Math.random() * guessNumRange);
+        console.log(correctNum);
         document.querySelector('.userScore').querySelector('span').textContent = score;
         document.querySelector('label').querySelector('span').textContent = `1 - ${guessNumRange}`;
         savetoLocalS({name: 'guessNumRange', value: guessNumRange});
+        savetoLocalS({name: 'correctNum', value: correctNum});
+        savetoLocalS({name: 'score', value: score});
+    } else {
+        if(numInput.value < correctNum) {
+            alert('incorrect, correct number is greater')
+        } else {
+            alert('incorrect, correct number is lower')
+        }
     }
 }
---
+++ level 106/homework/homework.js
@@ -0,0 +1,8 @@
+function sort(initialArray, sortingArray) {
+    let arr = Array.from(initialArray);
+
+    for(let i = 0; i < sortingArray.length; i++){
+       arr[sortingArray[i]] = initialArray[i]
+    }
+    return arr
+}
