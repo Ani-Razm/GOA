c28edc1598d213ba5e50ed172e2a35fd1abdec4e
Ani <158804664+an619@users.noreply.github.com>
2025-02-19 05:53:24 +0400

    GOA

--- level 112/homework/index.js
+++ level 112/homework/index.js
@@ -1,29 +1,27 @@
+// creating classes
 class Books {
-    constructor(title, pages, author){
+    constructor(title, pages, author) {
         this.title = title;
         this.pages = pages;
         this.author = author;
     }
 
     get info() {
-        return `${this.title}: ${this.pages} pages, by ${this.author}`;
+        return `${this.pages} pages, by ${this.author}`;
     }
+
     reading() {
-        console.log('reading ' + this.title);
+        return 'reading ' + this.title;
     }
+
 
     getTitle() {
         return this.title;
     }
 }
 
-const book = new Books("To Kill a Mockingbird", 200, "Harper Lee");
-
-console.log(book.getTitle());
-console.log(book.info);
-
 class Adventure extends Books {
-    constructor(title, pages, author, adventureType, year){
+    constructor(title, pages, author, adventureType, year) {
         super(title, pages, author);
         this.adventureType = adventureType;
         this.year = year;
@@ -34,22 +32,17 @@ class Adventure extends Books {
     }
 
     get info() {
-        return `${this.title}: ${this.pages} pages, by ${this.author}. year: ${this.year}; type: ${this.adventureType}.`;
+        return `${this.pages} pages, by ${this.author}. year: ${this.year}; type: ${this.adventureType}.`;
     }
 
     get AdventureType() {
         return this.adventureType;
-    }  
+    }
 }
 
-const book2 = new Adventure("The Lost City", "John Smith", 200, "treasure hunt", 2020);
-
-console.log(book2.getTitle());
-console.log(book2.info);
-console.log(book2.adventureType);
 
 class Fantasty extends Books {
-    constructor(title, pages, author, FantastyType, cover){
+    constructor(title, pages, author, FantastyType, cover) {
         super(title, pages, author);
         this.FantastyType = FantastyType;
         this.cover = cover;
@@ -60,22 +53,17 @@ class Fantasty extends Books {
     }
 
     get info() {
-        return `${this.title}: ${this.pages} pages, by ${this.author}. cover: ${this.cover}; type: ${this.FantastyType}.`;
+        return `${this.pages} pages, by ${this.author}. cover: ${this.cover}; type: ${this.FantastyType}.`;
     }
 
     get coverInfo() {
         return this.cover;
-    }  
+    }
 }
 
-const book3 = new Fantasty("The Lost City", "John Smith", 200, "Elderwood", "colorful");
-
-console.log(book3.getTitle());
-console.log(book3.info);
-console.log(book3.coverInfo);
 
 class Comedy extends Books {
-    constructor(title, pages, author, ComedyType, isPopular){
+    constructor(title, pages, author, ComedyType, isPopular) {
         super(title, pages, author);
         this.ComedyType = ComedyType;
         this.isPopular = isPopular;
@@ -91,11 +79,57 @@ class Comedy extends Books {
 
     get isKnown() {
         return this.isPopular ? "This is a very well-known book" : "This book isn't well-known";
-    }  
+    }
+
+    reading() {
+        return super.reading() + " with a smile";
+    }
 }
+
+
+// creating objects and testing
+const book = new Books("To Kill a Mockingbird", 200, "Harper Lee");
+
+console.log(book.getTitle());
+console.log(book.info);
+
+const book2 = new Adventure("The Lost City", "John Smith", 200, "treasure hunt", 2020);
+
+console.log(book2.getTitle());
+console.log(book2.info);
+console.log(book2.adventureType);
+
+const book3 = new Fantasty("The Lost City", "John Smith", 200, "Elderwood", "colorful");
+
+console.log(book3.getTitle());
+console.log(book3.info);
+console.log(book3.coverInfo);
 
 const book4 = new Comedy("The Lost City", "John Smith", 200, "dark-humor", true);
 
 console.log(book4.getTitle());
-console.log(book4.info);
+console.log(book4.reading());
 console.log(book4.isKnown);
+
+
+// function to display books
+function addBook(obj) {
+    let books = document.querySelector('.wrapper');
+    let book = document.createElement('div');
+    book.classList.add('book');
+    book.innerHTML = `
+    <img src="image.png" alt="book">
+    <h2>${obj.getTitle()}</h2>
+    <p>${obj.info}</p>
+    <button class="read-btn">read</button>
+    `;
+    books.appendChild(book);
+
+    book.querySelector('.read-btn').addEventListener('click', () => alert(obj.reading()));
+}
+
+let books = [book, book2, book3, book4];
+
+for (let i = 0; i < books.length; i++) {
+    addBook(books[i]);
+}
